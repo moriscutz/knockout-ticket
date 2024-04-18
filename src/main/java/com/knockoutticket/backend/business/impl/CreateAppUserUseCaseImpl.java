ec38d59ed31871da24a33ffabbf869d8c894749c
Valentin Morişcă <516431@student.fontys.nl>
2024-04-18 15:31:39 +0200

    Working again

--- src/main/java/com/knockoutticket/backend/business/impl/CreateAppUserUseCaseImpl.java
+++ src/main/java/com/knockoutticket/backend/business/impl/CreateAppUserUseCaseImpl.java
@@ -6,10 +6,12 @@ import com.knockoutticket.backend.domain.requests.CreateAppUserRequest;
 import com.knockoutticket.backend.domain.responses.CreateAppUserResponse;
 import com.knockoutticket.backend.persistence.AppUserRepository;
 import com.knockoutticket.backend.persistence.entity.AppUserEntity;
+import com.knockoutticket.backend.persistence.entity.UserTypeEntity;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -44,8 +46,20 @@ public class CreateAppUserUseCaseImpl implements CreateAppUserUseCase {
                 .password(encodedPassword)
                 .email(request.getEmail())
                 .build();
+        newAppUser.setUserRoles(Set.of(UserTypeEntity.builder()
+                .type(UserType.NORMAL_USER)
+                .user(newAppUser)
+                .build()));
 
-        newAppUser.addUserRole(UserType.NORMAL_USER);
+        if(newAppUser.getEmail() == null || newAppUser.getEmail().trim().isEmpty()) {
+            throw new IllegalArgumentException("Email must not be blank");
+        }
+        if (newAppUser.getUsername() == null || newAppUser.getUsername().trim().isEmpty()){
+            throw new IllegalArgumentException("Username must not be blank");
+        }
+        if (newAppUser.getPassword() == null || newAppUser.getPassword().trim().isEmpty()) {
+            throw new IllegalArgumentException("Password must not be blank");
+        }
         return appUserRepository.save(newAppUser);
     }
 }
--- src/main/java/com/knockoutticket/backend/persistence/EventOrganizerRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.knockoutticket.backend.persistence;
-//
-//import com.knockoutticket.backend.persistence.entity.EventOrganizerEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface EventOrganizerRepository extends JpaRepository<EventOrganizerEntity, Long> {
-//}
