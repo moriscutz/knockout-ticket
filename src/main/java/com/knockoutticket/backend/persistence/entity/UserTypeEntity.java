89c4872ee36590ff262c4f5b33b186661fdeed63
Valentin Morişcă <516431@student.fontys.nl>
2024-04-18 14:49:58 +0200

    Modified the DB schema

--- src/main/java/com/knockoutticket/backend/persistence/entity/UserTypeEntity.java
+++ src/main/java/com/knockoutticket/backend/persistence/entity/UserTypeEntity.java
@@ -1,8 +1,30 @@
 package com.knockoutticket.backend.persistence.entity;
 
-public enum UserTypeEntity {
-    NORMAL_USER,
-    BOXER,
-    EVENT_ORGANIZER,
-    ADMINISTRATOR
+import com.knockoutticket.backend.domain.models.UserType;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserTypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_name", nullable = false)
+    private UserType type;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @ToString.Exclude
+    private AppUserEntity user;
+
+    public UserTypeEntity(UserType type) {
+        this.type = type;
+    }
 }
