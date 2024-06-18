057762e9672e809fc156f8bbcfca7939005fc81f
Valentin Morişcă <516431@student.fontys.nl>
2024-06-18 21:05:26 +0200

    trying to deploy with docker.

--- src/main/resources/db/migration/V9__AddCustomerIdToBookings.sql
+++ src/main/resources/db/migration/V9__AddCustomerIdToBookings.sql
@@ -2,11 +2,5 @@ ALTER TABLE bookings ADD COLUMN customer_id BIGINT NOT NULL;
 
 
 ALTER TABLE bookings
-    DROP CONSTRAINT FKqferkk98p42soamfn02tqt6sm;
-
-ALTER TABLE bookings DROP COLUMN user_id;
-
-
-ALTER TABLE bookings
     ADD CONSTRAINT fk_customer
         FOREIGN KEY (customer_id) REFERENCES app_users(id);
